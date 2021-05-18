2560ca98826c4a7547c3ba5d8b8fe7c94d52521c
wangxiaoxiao <wangxiaoxiao45@jd.com>
2021-05-18 16:52:36 +0800

    v1

---
+++ db.js
@@ -0,0 +1,5 @@
+// database configuration, pull up as and when needed
+// using a local Mongo instance, start the 'mongod' daemon in your terminal
+module.exports = {
+  'url': 'mongodb+srv://wangxiaoxiao45:pwd139019_@cluster0.wfgpw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
